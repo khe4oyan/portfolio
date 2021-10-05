6387bad586ee3cfdf88db1fd92921c3c3d900991
khe4oyan <khe4oyan@bk.ru>
2021-10-05 20:36:42 +0400

    Add new fech

--- README.md
+++ README.md
@@ -1,8 +1,1 @@
-## Link: https://khe4oyans.github.io/portfolio/
-
-===============================================
-
-# v.1.0
-- The page is a set of projects that I made in the form of cards.
-- Each project has a separate card.
-- By clicking on the eye icon on the card, you can go and see the work
+# Link: https://khe4oyans.github.io/portfolio/
--- js/code.js
+++ js/code.js
@@ -1,35 +1,31 @@
-setListener();
 holiday();
+preloader();
 
-function setListener(){
-    const cards = document.querySelectorAll('.card');
-    cards.forEach((card) => {
-        card.addEventListener('mouseenter', function(){
-            let h = new Audio('../sound/a.wav');
-            h.volume = .2;
-            h.play();
-        });
-    });
-}
+function holiday() {
+  const now = new Date();
+  const month = now.getMonth() + 1;
+  const day = now.getDate();
 
-function holiday(){
-    const now = new Date();
-    const month = now.getMonth() + 1;
-    const day = now.getDate();
-    
-    switch(true){
-        // New Year set
-        case month == 1 && day <= 20 || month == 12 && day > 20:{
-            console.log('Load Ney Year theme');
-            let link = document.createElement('link');
-            link.rel = 'stylesheet';
-            link.href = 'holiday/newYear/newYear.css';
-            document.head.appendChild(link);
-            let logo = document.querySelector('header img');           
-            logo.src = 'holiday/newYear/logo.png';
-            break;
-        }
+  switch (true) {
+    // New Year set
+    case month == 1 && day <= 20 || month == 12 && day >= 20: {
+      console.log('Load Ney Year theme');
+      let link = document.createElement('link');
+      link.rel = 'stylesheet';
+      link.href = 'holiday/newYear/newYear.css';
+      document.head.appendChild(link);
+      let logo = document.querySelector('header img');
+      logo.src = 'holiday/newYear/logo.png';
+      break;
     }
+  }
 }
 
-// new year temp set
+function preloader(){
+  document.body.onload = function(){
+    setTimeout(() => {
+      document.querySelector('#preloader').remove();
+      document.querySelector('.preloader').classList.remove('preloader');
+    }, 1000);
+  }
+}
