1ee6bb21553fe6fbf8b36492e990c3b24fdd3cb3
khe4oyan <khe4oyan@bk.ru>
2022-01-11 21:50:03 +0400

    add

--- js/data.js
+++ js/data.js
@@ -18,8 +18,19 @@ function createCard(data){
     const img = new Image();
     img.src = card.img
 
+    const linker = document.createElement('div');
+    linker.classList.add('linker');
+
     const link = document.createElement('a');
     link.target = "_blank"
+
+    const git = document.createElement('a');
+    git.target = "_blank"
+    git.href = card.git;
+    const gitLogo = new Image();
+    gitLogo.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png';
+    git.appendChild(gitLogo);
+
 
     const tagI = document.createElement('i');
     tagI.classList.add('fas');
@@ -27,9 +38,12 @@ function createCard(data){
     link.appendChild(tagI);
     link.href = card.link;
 
+    linker.appendChild(git);
+    linker.appendChild(link);
+
     block.appendChild(img);
     block.appendChild(name);
-    block.appendChild(link);
+    block.appendChild(linker);
     cardsContainer.appendChild(block);
   }
 }
